2e318ee1f8c7b817c88d3b131e22b74e57e3b4c0
Gurpreet-Tech-Coder <71172450+Gurpreet-Tech-Coder@users.noreply.github.com>
2022-08-28 15:01:21 +0530

    create

---
+++ FindDuplicate.java
@@ -0,0 +1,25 @@
+import java.util.*;
+public class Main
+{
+    public static boolean finDuplicate(int arr[]){
+        for(int i=0;i<arr.length-1;i++){
+          int find=arr[i];
+          if(arr[i]==arr[i+1]){
+            return true;
+          }
+        }
+      return false;
+    }
+	public static void main(String[] args) {
+    Scanner sc=new Scanner(System.in);
+    int n=sc.nextInt();
+		int arr[]=new int[n];
+		System.out.println("Give input as integer");
+    for(int i=0;i<arr.length;i++){
+      arr[i]=sc.nextInt();
+    }
+    System.out.println(finDuplicate(arr));
+
+		
+	}
+}
