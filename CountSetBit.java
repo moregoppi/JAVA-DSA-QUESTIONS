79af5bb501b060aaf4a6708816deed0a4d231573
Gurpreet-Tech-Coder <71172450+Gurpreet-Tech-Coder@users.noreply.github.com>
2022-08-29 19:48:05 +0530

    create

---
+++ CountSetBit.java
@@ -0,0 +1,16 @@
+//time complexity:-O(log n)
+public class CountSetBit {
+    public static int countSetBits(int n){
+        int count=0;
+        while(n>0){
+            if((n&1)!=0){
+                count++;
+            }
+            n=n>>1;
+        }
+        return count;
+    }
+    public static void main(String[] args) {
+        System.out.println(countSetBits(6));
+    }
+}
