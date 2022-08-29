85559a81b6d610eed98890583b21cab2705d2cd6
Gurpreet-Tech-Coder <71172450+Gurpreet-Tech-Coder@users.noreply.github.com>
2022-08-29 18:35:29 +0530

    create

--- FindDuplicate.java
+++ FindDuplicate.java
@@ -13,13 +13,13 @@ public class Main
 	public static void main(String[] args) {
     Scanner sc=new Scanner(System.in);
     int n=sc.nextInt();
-		int arr[]=new int[n];
-		System.out.println("Give input as integer");
+    int arr[]=new int[n];
+    System.out.println("Give input as integer");
     for(int i=0;i<arr.length;i++){
       arr[i]=sc.nextInt();
     }
     System.out.println(finDuplicate(arr));
 
 		
-	}
+   }
 }
