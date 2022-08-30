611f1f1f190d3d29c07e5ed903556961ab9a1b8b
Gurpreet-Tech-Coder <71172450+Gurpreet-Tech-Coder@users.noreply.github.com>
2022-08-30 14:05:24 +0530

    create

--- Add 1 to an integer using Bit Manipulation.java
+++ Add 1 to an integer using Bit Manipulation.java
@@ -4,6 +4,6 @@ public class Add1ToAnIntegerUsingBitManipulation
 	public static void main(String[] args) {
 	    Scanner sc=new Scanner(System.in);
 		  int a=sc.nextInt();
-		  System.out.println(a|1);
+		  System.out.println(-~a);
 	}
 }
