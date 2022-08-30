eb040f9fb8319e4edd0179d62ff1bcfd839a377b
Gurpreet-Tech-Coder <71172450+Gurpreet-Tech-Coder@users.noreply.github.com>
2022-08-30 14:09:23 +0530

    create

---
+++ ConvertingUppercasetoLowercase.java
@@ -0,0 +1,9 @@
+import java.util.*;
+public class Main
+{
+	public static void main(String[] args) {
+	    for(char ch='a';ch<='z';ch++){
+	        System.out.println((char)(ch|' '));
+	    }
+	}
+}
