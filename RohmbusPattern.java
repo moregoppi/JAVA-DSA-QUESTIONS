7ca0aa0db5746f93c011a0df6d6d3f51b0616d7c
Gurpreet Singh <71172450+Gurpreet-Tech-Coder@users.noreply.github.com>
2022-09-11 23:48:34 +0530

    create

---
+++ RohmbusPattern.java
@@ -0,0 +1,19 @@
+public class RohmbusPattern {
+    public static void rOhmbus(int n){
+        for(int i=1;i<=n;i++){
+            //spaces
+            for(int j=1;j<=n-i;j++){
+                System.out.print(" ");
+            }
+            //stars
+            for(int j=1;j<=n;j++){
+                System.out.print("*");
+            }
+            System.out.println();
+        }
+    }
+     public static void main (String[] args) {
+         rOhmbus(4);
+     }
+    
+}
