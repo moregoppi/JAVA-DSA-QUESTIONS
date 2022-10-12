049c44ab5a77e8caa7975d68e0e68b7dfe54a7db
Gurpreet Singh <71172450+moregoppi@users.noreply.github.com>
2022-10-12 21:10:59 +0530

    create

---
+++ LinkedListUsingStack.java
@@ -0,0 +1,60 @@
+import java.util.Stack;
+
+public class linkedlistusingstack {
+    static class Node{
+        int data;
+        Node next;
+        Node(int data){
+            this.data = data;
+            this.next = null;
+        }
+    }
+    static class stack{
+        static Node head = null;
+
+        public static boolean isEmpty(){
+            return head == null;
+        }
+
+        //push
+        public static void push(int data){
+            Node newnode = new Node(data);
+
+            if(isEmpty()){
+                return;
+            }
+
+            newnode.next = head;
+            head = newnode;
+        }
+
+        //pop
+        public static int pop(int data){
+            if(isEmpty()){
+                return -1;
+            }
+
+            int top = head.data;
+            head = head.next;
+            return top;
+        }
+
+        //peek
+        public static int peek(){
+            if(isEmpty()){
+                return -1;
+            }
+            return head.data;
+        }
+    }
+    public static void main(String[] args) {
+        Stack s = new Stack();
+        s.push(1);
+        s.push(2);
+        s.push(3);
+        while(!s.isEmpty()){
+            System.out.println(s.peek());
+            s.pop();
+        }
+    }
+}
