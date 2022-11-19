e3781626d347d3ffce70c68e1fe8b9b4792f71f1
Gurpreet Singh <71172450+moregoppi@users.noreply.github.com>
2022-11-19 16:10:54 +0530

    create

--- ReverseInteger.java
+++ ReverseInteger.java
@@ -1,13 +1,15 @@
-public class Main
-{
-	public static void main(String[] args) {
-	    int n=1534236469;
-	    int rev=0;
-		while(n!=0){
-		    int lastdigit=n%10;
-		    rev=(rev*10)+lastdigit;
-		    n=n/10;
-		}
-		System.out.println(rev);
-	}
+class Solution {
+    public int reverse(int x) {
+        long num = 0;
+        while(x!=0){
+            int lastdigit = x%10;
+            num = (num*10) + lastdigit;
+            x /= 10;
+        }
+        if(num > Integer.MAX_VALUE || num < Integer.MIN_VALUE){
+            return 0;
+        }
+        
+        return (int)num;
+    }
 }
