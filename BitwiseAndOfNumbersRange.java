81feff235d46f207d8271e770be83df95713f245
Gurpreet Singh <71172450+moregoppi@users.noreply.github.com>
2022-11-26 15:24:46 +0530

    create

---
+++ BitwiseAndOfNumbersRange.java
@@ -0,0 +1,36 @@
+// 201. Bitwise AND of Numbers Range
+// Medium
+// 2.6K
+// 192
+// Companies
+// Given two integers left and right that represent the range [left, right], return the bitwise AND of all numbers in this range, inclusive.
+
+ 
+
+// Example 1:
+
+// Input: left = 5, right = 7
+// Output: 4
+// Example 2:
+
+// Input: left = 0, right = 0
+// Output: 0
+// Example 3:
+
+// Input: left = 1, right = 2147483647
+// Output: 0
+
+
+
+
+class Solution {
+    public int rangeBitwiseAnd(int left, int right) {
+        int i=0; 
+        while(left != right){
+            left >>= 1;
+            right >>= 1;
+            i++;
+        }
+        return left << i;
+    }
+}
