7f9cab6621cd2eb46a9c285ec4776197f9e61e7d
Gurpreet Singh <71172450+moregoppi@users.noreply.github.com>
2022-12-08 21:45:11 +0530

    create

--- BestTimeToBuyStocks.java
+++ BestTimeToBuyStocks.java
@@ -1,24 +1,28 @@
-// 409. Longest Palindrome
+// 121. Best Time to Buy and Sell Stock
 // Easy
-// 4K
-// 236
+// 21.8K
+// 690
 // Companies
-// Given a string s which consists of lowercase or uppercase letters, return the length of the longest palindrome that can be built with those letters.
+// You are given an array prices where prices[i] is the price of a given stock on the ith day.
 
-// Letters are case sensitive, for example, "Aa" is not considered a palindrome here.
+// You want to maximize your profit by choosing a single day to buy one stock and choosing a different day in the future to sell that stock.
+
+// Return the maximum profit you can achieve from this transaction. If you cannot achieve any profit, return 0.
 
  
 
 // Example 1:
 
-// Input: s = "abccccdd"
-// Output: 7
-// Explanation: One longest palindrome that can be built is "dccaccd", whose length is 7.
+// Input: prices = [7,1,5,3,6,4]
+// Output: 5
+// Explanation: Buy on day 2 (price = 1) and sell on day 5 (price = 6), profit = 6-1 = 5.
+// Note that buying on day 2 and selling on day 1 is not allowed because you must buy before you sell.
 // Example 2:
 
-// Input: s = "a"
-// Output: 1
-// Explanation: The longest palindrome that can be built is "a", whose length is 1.
+// Input: prices = [7,6,4,3,1]
+// Output: 0
+// Explanation: In this case, no transactions are done and the max profit = 0.
+
 
 
 class Solution {
