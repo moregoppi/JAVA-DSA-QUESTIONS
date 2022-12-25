d890059aa2431674f2b8dd13b865d175af509c56
Gurpreet Singh <71172450+moregoppi@users.noreply.github.com>
2022-12-25 17:50:18 +0530

    create

--- Diamter of a Tree.java
+++ Diamter of a Tree.java
@@ -47,6 +47,30 @@ public class diameteroftree {
 
         return Math.max(selfDiameter, Math.max(leftdiameter, rightdiameter));
     }
+    //Approach 2:-
+    static class Info{  //T.c:- O(n) because in this we are calculating both at same at same node
+        int diam;
+        int ht;
+
+        public Info(int diam, int ht){
+            this.diam = diam;
+            this.ht = ht;
+        }
+    }
+    public static Info diameter(Node root){
+        if(root == null){
+            return new Info(0, 0);
+        }
+        Info leftInfo = diameter(root.left);
+        Info rightInfo = diameter(root.right);
+
+        int diam = Math.max(Math.max(leftInfo.diam, rightInfo.diam), leftInfo.ht + rightInfo.ht + 1);
+        int ht = Math.max(leftInfo.ht, rightInfo.ht) + 1;
+
+        return new Info(diam, ht);
+
+    }
+
 
     public static void main(String[] args) {
         Node root = new Node(1);
