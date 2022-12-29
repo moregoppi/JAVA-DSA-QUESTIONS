f345f4b9b205da78b04bf1824d718ce909bce263
Gurpreet Singh <71172450+moregoppi@users.noreply.github.com>
2022-12-29 17:16:31 +0530

    create

--- Lowest Comman Ancestor.java
+++ Lowest Comman Ancestor.java
@@ -11,7 +11,8 @@ public class lowestcommanancestor {
             this.right = null;
         }
     }
-
+//     -------------------------------------------------------------------------------------------------------------------
+    //Approach 1:
     public static boolean getPath(Node root, int n, ArrayList<Node> path){
         if(root == null){
             return false;
@@ -52,6 +53,27 @@ public class lowestcommanancestor {
         return node;
 
     }
+// ----------------------------------------------------------------------------------------------------------------
+    //Approach 2:
+    public static Node LCA(Node root, int n1, int n2){
+        if(root == null || root.data == n1 || root.data == n2){
+            return root;
+        }
+
+        Node leftlca = LCA(root.left, n1, n2);
+        Node rightlca = LCA(root.right, n1, n2);
+
+        if(rightlca == null){
+            return leftlca;
+        }
+        if(leftlca == null){
+            return rightlca;
+        }
+
+        return root;
+    }
+    // ------------------------------------------------------------------------------------
+    
     public static void main(String[] args) {
         Node root = new Node(1);
         root.left = new Node(2);
@@ -61,8 +83,8 @@ public class lowestcommanancestor {
         root.right.left = new Node(6);
         root.right.right = new Node(7);
 
-        int n1 = 2, n2 = 3;
+        int n1 = 4, n2 = 5;
 
-        System.out.println(lca(root, n1, n2).data);
+        System.out.println(LCA(root, n1, n2).data);
     }
 }
