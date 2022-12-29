29cac8058b61d803154fd7e4e90f8b504d304e46
Gurpreet Singh <71172450+moregoppi@users.noreply.github.com>
2022-12-29 23:58:22 +0530

    create

---
+++ Permutatuion Sequence.java
@@ -0,0 +1,62 @@
+// 60. Permutation Sequence
+// Hard
+// 5.1K
+// 427
+// Companies
+// The set [1, 2, 3, ..., n] contains a total of n! unique permutations.
+
+// By listing and labeling all of the permutations in order, we get the following sequence for n = 3:
+
+// "123"
+// "132"
+// "213"
+// "231"
+// "312"
+// "321"
+// Given n and k, return the kth permutation sequence.
+
+ 
+
+// Example 1:
+
+// Input: n = 3, k = 3
+// Output: "213"
+// Example 2:
+
+// Input: n = 4, k = 9
+// Output: "2314"
+// Example 3:
+
+// Input: n = 3, k = 1
+// Output: "123"
+
+
+class Solution {
+    public String getPermutation(int n, int k) {
+        StringBuilder sb = new StringBuilder();
+        ArrayList<Integer> nums = new ArrayList<>();
+        int fact[] = new int[n+1];  //fact[i] = i!(factorial)
+
+        for(int i=1; i<=n; i++){
+            nums.add(i);
+        }
+
+        Arrays.fill(fact, 1);
+
+        for(int i=2; i <= n; ++i){
+            fact[i] = fact[i-1] * i;
+        }
+
+        --k;
+
+        for(int i=n-1; i>=0; --i){
+            final int j = k / fact[i];
+            k %= fact[i];
+            sb.append(nums.get(j));
+            nums.remove(j);
+        }
+
+        return sb.toString();
+
+    }
+}
