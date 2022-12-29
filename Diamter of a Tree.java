480e74138fb5656a243163f61391c46d71a4e1ea
Gurpreet Singh <71172450+moregoppi@users.noreply.github.com>
2022-12-29 18:26:00 +0530

    create

--- Diamter of a Tree.java
+++ Diamter of a Tree.java
@@ -1,3 +1,83 @@
+// 543. Diameter of Binary Tree
+// Easy
+// 10.3K
+// 653
+// Companies
+// Given the root of a binary tree, return the length of the diameter of the tree.
+
+// The diameter of a binary tree is the length of the longest path between any two nodes in a tree. This path may or may not pass through the root.
+
+// The length of a path between two nodes is represented by the number of edges between them.
+
+ 
+
+// Example 1:
+
+
+// Input: root = [1,2,3,4,5]
+// Output: 3
+// Explanation: 3 is the length of the path [4,2,1,3] or [5,2,1,3].
+// Example 2:
+
+// Input: root = [1,2]
+// Output: 1
+
+
+/**
+ * Definition for a binary tree node.
+ * public class TreeNode {
+ *     int val;
+ *     TreeNode left;
+ *     TreeNode right;
+ *     TreeNode() {}
+ *     TreeNode(int val) { this.val = val; }
+ *     TreeNode(int val, TreeNode left, TreeNode right) {
+ *         this.val = val;
+ *         this.left = left;
+ *         this.right = right;
+ *     }
+ * }
+ */
+class Solution {
+    public static int heightofTree(TreeNode root){
+        if(root == null){
+            return 0;
+        }
+
+        int lh = heightofTree(root.left);
+        int rh = heightofTree(root.right);
+
+        return Math.max(lh, rh) + 1;
+    }
+    public int diameterOfBinaryTree(TreeNode root) {
+        if(root == null){
+            return 0;
+        }
+
+        int leftdiameter = diameterOfBinaryTree(root.left);
+        int lh = heightofTree(root.left);
+        int rightdiameter = diameterOfBinaryTree(root.right);
+        int rh = heightofTree(root.right);
+
+        int selfdiameter = lh + rh;
+
+        return Math.max(selfdiameter, Math.max(leftdiameter, rightdiameter));
+    
+    }
+}
+
+
+
+
+// --------------------------------------------------------------------------------------------------------------------------------------------------------
+
+
+
+
+
+
+
+
 public class diameteroftree {
     static class Node{
         int data;
