4a38f3fed0f6e8bac19bd79cd006ec2166d3f185
Gurpreet Singh <71172450+moregoppi@users.noreply.github.com>
2022-12-30 00:22:25 +0530

    create

---
+++ Right View of Binary Tree.java
@@ -0,0 +1,69 @@
+// 199. Binary Tree Right Side View
+// Medium
+// 9.2K
+// 546
+// Companies
+// Given the root of a binary tree, imagine yourself standing on the right side of it, return the values of the nodes you can see ordered from top to bottom.
+
+ 
+
+// Example 1:
+
+
+// Input: root = [1,2,3,null,5,null,4]
+// Output: [1,3,4]
+// Example 2:
+
+// Input: root = [1,null,3]
+// Output: [1,3]
+// Example 3:
+
+// Input: root = []
+// Output: []
+
+
+
+/**
+ * Definition for a binary tree node.
+ * public class TreeNode {
+ *     int val;
+ *     TreeNode left;
+ *     TreeNode right;
+ *     TreeNode() {}
+ *     TreeNode(int val) { this.val = val; }
+ *     TreeNode(int val, TreeNode left, TreeNode right) {
+ *         this.val = val;
+ *         this.left = left;
+ *         this.right = right;
+ *     }
+ * }
+ */
+class Solution {
+    public List<Integer> rightSideView(TreeNode root) {
+        List<Integer> list = new ArrayList<>();
+        if(root == null){
+            return list;
+        }
+        Queue<TreeNode> q = new LinkedList<>();
+        q.add(root);
+
+        while(!q.isEmpty()){
+            int n = q.size();
+
+            for(int i=0; i<n; i++){
+                TreeNode curr = q.remove();
+
+                if(i == n-1){
+                    list.add(curr.val);
+                }
+                if(curr.left != null){
+                    q.add(curr.left);
+                }
+                if(curr.right != null){
+                    q.add(curr.right);
+                }
+            }
+        }
+        return list;
+    }
+}
