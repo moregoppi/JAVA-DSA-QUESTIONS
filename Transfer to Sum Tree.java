977c1778bdd2c37172815f7b34bdaa83ce96d2e9
Gurpreet Singh <71172450+moregoppi@users.noreply.github.com>
2022-12-30 16:50:34 +0530

    create

---
+++ Transfer to Sum Tree.java
@@ -0,0 +1,55 @@
+public class transforsumofnodes {
+    static class Node{
+        int data;
+        Node left;
+        Node right;
+
+        Node(int data){
+            this.data = data;
+            this.left = null;
+            this.right = null;
+        }
+    }
+
+    public static int transferingSum(Node root){
+        if(root == null){
+            return 0;
+        }
+
+        int leftChild = transferingSum(root.left);
+        int rightChild = transferingSum(root.right);
+
+        int data = root.data;
+
+        int newLeft = root.left == null ? 0 : root.left.data;
+        int newRight = root.right == null ? 0 : root.right.data;
+
+        root.data = newLeft + leftChild + newRight + rightChild;
+
+        return data;
+    }
+
+    public static void preOrder(Node root){
+        if(root == null){
+            return;
+        }
+
+        System.out.print(root.data+" ");
+
+        preOrder(root.left);
+        preOrder(root.right);
+    }
+    public static void main(String[] args) {
+        Node root = new Node(1);
+        root.left = new Node(2);
+        root.right = new Node(3);
+        root.left.left = new Node(4);
+        root.left.right = new Node(5);
+        root.right.left = new Node(6);
+        root.right.right = new Node(7);
+        
+        transferingSum(root);
+
+        preOrder(root);
+    }
+}
