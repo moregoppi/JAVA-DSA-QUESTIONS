b779f3d2772de6d98bc9f4b30e8f4a79a4086ff5
Gurpreet Singh <71172450+moregoppi@users.noreply.github.com>
2023-01-03 18:19:32 +0530

    create

---
+++ Build Binary Search Tree.java
@@ -0,0 +1,52 @@
+public class buildbst {
+    static class Node{
+        int data;
+        Node right;
+        Node left;
+
+        Node(int data){
+            this.data = data;
+            this.left = null;
+            this.right = null;
+        }
+    }
+
+    public static Node buildBST(Node root, int val){
+        if(root == null){
+            root = new Node(val);
+            return root;
+        }
+
+        if(root.data > val){
+            //left subtree
+            root.left = buildBST(root.left, val);
+        }else{
+            //right subtree
+            root.right = buildBST(root.right, val);
+        }
+
+        return root;
+    }
+
+    public static void inOrder(Node root){
+        if(root == null){
+            return;
+        }
+
+        inOrder(root.left);
+        System.out.print(root.data+" ");
+        inOrder(root.right);
+    }
+    
+    public static void main(String[] args) {
+        int values[] = {5,1,3,4,2,7};
+        Node root = null;
+
+        for(int i=0; i<values.length; i++){
+            root = buildBST(root, values[i]);
+        }
+
+        inOrder(root);
+        System.out.println();
+    }    
+}
