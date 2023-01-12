ca7941b81184220b332fc377bb7b34d976c42612
Gurpreet Singh <71172450+moregoppi@users.noreply.github.com>
2023-01-12 18:19:59 +0530

    create

---
+++ Find the Pivot Integer.java
@@ -0,0 +1,35 @@
+// 2485. Find the Pivot Integer
+// Easy
+// 243
+// 4
+// Companies
+// Given a positive integer n, find the pivot integer x such that:
+
+// The sum of all elements between 1 and x inclusively equals the sum of all elements between x and n inclusively.
+// Return the pivot integer x. If no such integer exists, return -1. It is guaranteed that there will be at most one pivot index for the given input.
+
+ 
+
+// Example 1:
+
+// Input: n = 8
+// Output: 6
+// Explanation: 6 is the pivot integer since: 1 + 2 + 3 + 4 + 5 + 6 = 6 + 7 + 8 = 21.
+// Example 2:
+
+// Input: n = 1
+// Output: 1
+// Explanation: 1 is the pivot integer since: 1 = 1.
+// Example 3:
+
+// Input: n = 4
+// Output: -1
+// Explanation: It can be proved that no such integer exist.
+
+class Solution {
+    public int pivotInteger(int n) {
+        final int y = (n * n + n)/2;
+        final int x = (int)Math.sqrt(y);
+        return x * x == y ? x : -1;
+    }
+}
