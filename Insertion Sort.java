05630bcfc88279273438ff7be1e359da88926055
Gurpreet Singh <71172450+moregoppi@users.noreply.github.com>
2023-01-13 15:38:04 +0530

    create

---
+++ Insertion Sort.java
@@ -0,0 +1,27 @@
+import java.util.*;
+public class insertionsort {
+    public static void sorting(int[] array){
+        for(int i=0;i<array.length;i++){
+            System.out.print(array[i]+" ");
+        }
+        System.out.println();
+    }
+    public static void main(String[] args) {
+        Scanner sc=new Scanner(System.in);
+        int size=sc.nextInt();
+        int[] array=new int[size];
+        for(int i=0;i<size;i++){
+            array[i]=sc.nextInt();
+        }
+        for(int i=1;i<array.length;i++){    //too run in unsorted array
+            int current=array[i];       
+            int j=i-1;                 // first element of unsorted part
+            while(j>=0 && current < array[j]){   //when array[j] is greater than current element then we can inserte it
+                array[j+1]=array[j];          //here we are inserting the element the element
+                j--;
+            }
+            array[j+1]=current;
+        }
+        sorting(array);
+    }
+}
