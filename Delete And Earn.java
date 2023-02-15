32e65193de477bc39b76b765fb28b59c5d404dd8
Gurpreet Singh <71172450+moregoppi@users.noreply.github.com>
2023-02-15 19:04:40 +0530

    create

--- Delete And Earn.java
+++ Delete And Earn.java
@@ -8,8 +8,6 @@
 // Pick any nums[i] and delete it to earn nums[i] points. Afterwards, you must delete every element equal to nums[i] - 1 and every element equal to nums[i] + 1.
 // Return the maximum number of points you can earn by applying the above operation some number of times.
 
- 
-
 // Example 1:
 
 // Input: nums = [3,4,2]
