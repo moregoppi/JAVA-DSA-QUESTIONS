328da1f298731609512e369b4817bb5a827b1de4
Gurpreet Singh <71172450+moregoppi@users.noreply.github.com>
2023-03-09 20:42:05 +0530

    create

--- k Closest Points to Origin.java
+++ k Closest Points to Origin.java
@@ -58,3 +58,45 @@ class Solution {
         return arr;
     }
 }
+
+
+
+
+
+
+// Using PriorityQueue
+import java.awt.Point;
+import java.util.*;
+public class nearbycars {
+    static class Point implements Comparable<Point>{
+        int x;
+        int y;
+        int distSq;
+        int indx;
+
+        public Point(int x, int y, int distSq, int indx){
+            this.x = x;
+            this.y = y;
+            this.distSq = distSq;
+            this.indx = indx;
+        }
+
+        @Override
+        public int compareTo(Point p2){
+            return this.distSq - p2.distSq;
+        }
+    }
+    public static void main(String[] args) {
+        int[][] arr = {{3,3}, {3,-1}, {2,4}};
+        int k = 2;
+        PriorityQueue<Point> pq =new PriorityQueue<>();
+        for(int i=0; i<arr.length; i++){
+            int distSq = (arr[i][0] * arr[i][0]) + (arr[i][1] * arr[i][1]);
+            pq.add(new Point(arr[i][0], arr[i][1], distSq, i));
+        }
+
+        for(int i=0; i<k; i++){
+            System.out.println("C"+pq.remove().indx);
+        }
+    }
+}
