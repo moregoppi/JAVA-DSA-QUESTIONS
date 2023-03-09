f18b0a8ec1e045966aaf5af76e017f9f2ca959bb
Gurpreet Singh <71172450+moregoppi@users.noreply.github.com>
2023-03-09 20:25:36 +0530

    create

---
+++ k Closest Points to Origin.java
@@ -0,0 +1,60 @@
+// 973. K Closest Points to Origin
+// Medium
+// 7.2K
+// 260
+// Companies
+// Given an array of points where points[i] = [xi, yi] represents a point on the X-Y plane and an integer k, return the k closest points to the origin (0, 0).
+
+// The distance between two points on the X-Y plane is the Euclidean distance (i.e., √(x1 - x2)2 + (y1 - y2)2).
+
+// You may return the answer in any order. The answer is guaranteed to be unique (except for the order that it is in).
+
+ 
+
+// Example 1:
+
+
+// Input: points = [[1,3],[-2,2]], k = 1
+// Output: [[-2,2]]
+// Explanation:
+// The distance between (1, 3) and the origin is sqrt(10).
+// The distance between (-2, 2) and the origin is sqrt(8).
+// Since sqrt(8) < sqrt(10), (-2, 2) is closer to the origin.
+// We only want the closest k = 1 points from the origin, so the answer is just [[-2,2]].
+// Example 2:
+
+// Input: points = [[3,3],[5,-1],[-2,4]], k = 2
+// Output: [[3,3],[-2,4]]
+// Explanation: The answer [[-2,4],[3,3]] would also be accepted.
+ 
+
+// Constraints:
+
+// 1 <= k <= points.length <= 104
+// -104 < xi, yi < 104
+
+
+
+class Solution {
+    public int[][] kClosest(int[][] points, int k) {
+        int[][] arr = new int[k][2];
+
+        int distance[][] = new int[points.length][2];
+
+		for(int i=0; i<points.length; i++){
+		    int distance1 = (points[i][0] * points[i][0]) + (points[i][1] * points[i][1]);
+		    distance[i][1] = distance1;
+		    distance[i][0] = i;
+		    
+		}
+		
+		Arrays.sort(distance, Comparator.comparingDouble(o -> o[1]));
+
+        for(int i=0; i<k; i++){
+            arr[i][0] = points[distance[i][0]][0];
+            arr[i][1] = points[distance[i][0]][1];
+        }
+
+        return arr;
+    }
+}
