5106c5791ea8325c33eb51db9131f9754d4b96af
Gurpreet Singh <71172450+moregoppi@users.noreply.github.com>
2023-03-26 17:05:23 +0530

    create

--- Insert and Search Operatio on Trie.java
+++ Insert and Search Operatio on Trie.java
@@ -30,12 +30,20 @@ public class insertionintrie {
         curr.endofword = true;
     }
     // for searching a key we are going to traverse through whole key by charcters and check wisely a character is present at that level or not 
+    // if that chacacter is not present on level then we are going to return false and for true we are going to check if endofword is true or not
+    // if its true its mean all character of key are present on trie
     public static boolean search(String key){
         Node curr = root;
 
         for(int level=0; level<key.length(); level++){
-
+            int idx = key.charAt(level) - 'a';
+            if(curr.children[idx] == null){
+                return false;
+            }
+            curr = curr.children[idx];
         }
+
+        return curr.endofword == true;
     }
     public static void main(String[] args) {
         String[] words = {"thee", "there", "their", "a", "then", "any"};
@@ -43,5 +51,8 @@ public class insertionintrie {
         for(int i=0; i<words.length; i++){
             insertCharacter(words[i]);
         }
+
+        System.out.println(search("their"));
+        System.out.println(search("them"));
     }    
 }
