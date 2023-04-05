27a6771b1871e710004fac28ff9906942150d5f2
Gurpreet Singh <71172450+moregoppi@users.noreply.github.com>
2023-04-05 15:27:16 +0530

    create

--- Power of three.java
+++ Power of three.java
@@ -26,6 +26,26 @@
 // Explanation: There is no x where 3x = (-1).
 
 
+
+// Recursive method
+class Solution {
+    public boolean isPowerOfThree(int n) {
+        if(n <= 0){
+            return false;
+        }
+        if(n % 3 == 0){
+            return isPowerOfThree(n / 3);
+        }
+        if(n == 1){
+            return true;
+        }
+        return false;
+    }
+}
+
+
+
+// Complicate method
 class Solution {
     public boolean isPowerOfThree(int n) {
         int a = 3;
