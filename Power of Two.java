8bab68598436bc1f651754423cd968d3fc09dc75
Gurpreet Singh <71172450+moregoppi@users.noreply.github.com>
2023-04-05 15:29:57 +0530

    create

--- Power of Two.java
+++ Power of Two.java
@@ -24,7 +24,24 @@
 // Input: n = 3
 // Output: false
 
+//  recursive method
+class Solution {
+    public boolean isPowerOfTwo(int n) {
+        if(n <= 0){
+            return false;
+        }
+        if(n % 2 == 0){
+            return isPowerOfTwo(n / 2);
+        }
+        if(n == 1){
+            return true;
+        }
+        return false;
+    }
+}
 
+
+// Iterative method
 class Solution {
     public boolean isPowerOfTwo(int n) {
         if(n == 8){
