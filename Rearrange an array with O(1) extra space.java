069030ccf5813db200f8630f84d3ead766bbfd6d
Gurpreet Singh <71172450+moregoppi@users.noreply.github.com>
2023-06-19 16:35:04 +0530

    create

--- Rearrange an array with O(1) extra space.java
+++ Rearrange an array with O(1) extra space.java
@@ -35,7 +35,7 @@
 
 
 
-
+// Solution 1:
 //User function Template for Java
 
 class Solution
@@ -56,6 +56,33 @@ class Solution
         
         for(int i=0; i<n; i++){
             arr[i] = temp[i];
+        }
+    }
+}
+
+
+
+
+// Solution 2
+//User function Template for Java
+
+class Solution
+{
+ 
+    // arr: input array
+    // n: size of array
+    //Function to rearrange an array so that arr[i] becomes arr[arr[i]]
+    //with O(1) extra space.
+    static void arrange(long arr[], int n)
+    {
+        // your code here
+        long temp[] = new long[n];
+        for(int i=0; i<n; i++){
+            arr[i] += (arr[(int)arr[i]] % n) * n;
+        }
+        
+        for(int i=0; i<n; i++){
+            arr[i] /= n;
         }
     }
 }
