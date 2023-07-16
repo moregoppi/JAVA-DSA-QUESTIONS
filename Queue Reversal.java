cef7624b24566d4877f47b3758e909884a72edb2
Gurpreet Singh <71172450+moregoppi@users.noreply.github.com>
2023-07-16 15:50:45 +0530

    create

---
+++ Queue Reversal.java
@@ -0,0 +1,53 @@
+// Queue Reversal
+// BasicAccuracy: 77.98%Submissions: 83K+Points: 1
+// POTD July Placement Special : All POTD in the month of July are based on popular interview questions. Solve every day to ace the upcoming Placement Season !
+// Registered
+// Given a Queue Q containing N elements. The task is to reverse the Queue. Your task is to complete the function rev(), that reverses the N elements of the queue.
+
+// Example 1:
+
+// Input:
+// 6
+// 4 3 1 10 2 6
+// Output: 
+// 6 2 10 1 3 4
+// Explanation: 
+// After reversing the given elements of the queue , the resultant queue will be 6 2 10 1 3 4.
+// Example 2:
+
+// Input:
+// 4
+// 4 3 2 1 
+// Output: 
+// 1 2 3 4
+// Explanation: 
+// After reversing the given elements of the queue , the resultant queue will be 1 2 3 4.
+// Your Task: You need to complete the function rev that takes a queue as parameter and returns the reversed queue. The printing is done automatically by the driver code.
+
+// Expected Time Complexity : O(n)
+// Expected Auxilliary Space : O(n)
+
+// Constraints:
+// 1 ≤ N ≤ 105
+// 1 ≤ elements of Queue ≤ 105
+
+
+
+
+//User function Template for Java
+/*Complete the function below*/
+class GfG{
+    //Function to reverse the queue.
+    public Queue<Integer> rev(Queue<Integer> q){
+        //add code here.
+        Stack<Integer> s = new Stack<>();
+        while(!q.isEmpty()){
+            s.add(q.remove());
+        }
+        
+        while(!s.isEmpty()){
+            q.add(s.pop());
+        }
+        return q;
+    }
+}
