c0fa696125f20cbcc10f9160c87019fe901a65bb
Gurpreet Singh <71172450+moregoppi@users.noreply.github.com>
2023-08-20 15:11:23 +0530

    create

---
+++ Number of occurrence.java
@@ -0,0 +1,55 @@
+// Number of occurrence
+// MediumAccuracy: 59.34%Submissions: 145K+Points: 4
+// Applying for Jobs, Now Easier Than Today's Problem! Explore Now
+
+// Given a sorted array Arr of size N and a number X, you need to find the number of occurrences of X in Arr.
+
+// Example 1:
+
+// Input:
+// N = 7, X = 2
+// Arr[] = {1, 1, 2, 2, 2, 2, 3}
+// Output: 4
+// Explanation: 2 occurs 4 times in the
+// given array.
+// Example 2:
+
+// Input:
+// N = 7, X = 4
+// Arr[] = {1, 1, 2, 2, 2, 2, 3}
+// Output: 0
+// Explanation: 4 is not present in the
+// given array.
+// Your Task:
+// You don't need to read input or print anything. Your task is to complete the function count() which takes the array of integers arr, n, and x as parameters and returns an integer denoting the answer.
+// If x is not present in the array (arr) then return 0.
+
+// Expected Time Complexity: O(logN)
+// Expected Auxiliary Space: O(1)
+
+// Constraints:
+// 1 ≤ N ≤ 105
+// 1 ≤ Arr[i] ≤ 106
+// 1 ≤ X ≤ 106
+
+
+
+
+//User function Template for Java
+
+
+
+class Solution {
+    int count(int[] arr, int n, int x) {
+        // code here
+        HashMap<Integer, Integer> mp = new HashMap<>();
+        for(int i=0; i<n; i++){
+            mp.put(arr[i], mp.getOrDefault(arr[i], 0)+1);
+        }
+        
+        if(mp.containsKey(x)){
+            return mp.get(x);
+        }
+        return 0;
+    }
+}
