afb3b4cbcc06c545c3887870b8d61187dcf2343a
Gurpreet Singh <71172450+moregoppi@users.noreply.github.com>
2023-10-02 15:30:52 +0530

    Updated Leetode POTD 02-10-23

--- Remove Colored Pieces if Both Neighbors are the Same Color.java
+++ Remove Colored Pieces if Both Neighbors are the Same Color.java
@@ -76,7 +76,7 @@ class Solution {
     public boolean winnerOfGame(String colors) {
         int countA = 0;
         int countB = 0;
-        for(int i=1; i+1<colors.length(); ++i){
+        for(int i=1; i<colors.length()-1; ++i){
             if(colors.charAt(i-1) == colors.charAt(i) && colors.charAt(i+1) == colors.charAt(i)){
                 if(colors.charAt(i) == 'A'){
                     countA++;
